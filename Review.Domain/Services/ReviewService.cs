5b70298f20eb9235143d01d85391895c47382fec
dels-in <chernenko3954@mail.ru>
2023-12-20 20:05:05 +0300

    обновил проставление среднего рейтинга при добавлении нового отзыва

--- Review.Domain/Services/ReviewService.cs
+++ Review.Domain/Services/ReviewService.cs
@@ -27,6 +27,9 @@ public class ReviewService : IReviewService
         try
         {
             var dateTime = DateTime.Now;
+            var grades = await _databaseContext.Feedbacks.Select(x => x.Grade).ToListAsync();
+            grades.Add(grade);
+            var gradesAverage = grades.Average();
 
             var rating = await _databaseContext.Ratings.FirstOrDefaultAsync(r => r.ProductId == productId);
             if (rating == null)
@@ -35,9 +38,15 @@ public class ReviewService : IReviewService
                 {
                     ProductId = productId,
                     CreateDate = dateTime,
-                    Grade = grade,
+                    Grade = gradesAverage,
                 };
                 _databaseContext.Ratings.Add(rating);
+                await _databaseContext.SaveChangesAsync();
+            }
+            else
+            {
+                rating.Grade = gradesAverage;
+                await _databaseContext.SaveChangesAsync();
             }
 
             var feedback = new Feedback
